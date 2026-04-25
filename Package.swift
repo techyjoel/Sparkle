4a025e9ed9377add1abb6ce517692f3819c08d7f
Joel Mulkey <techyjoel@gmail.com>
2026-04-25 11:51:32 -0700

    Point SPM package at Breakdown Sparkle artifact
    Created with AI assistance

--- Package.swift
+++ Package.swift
@@ -4,9 +4,9 @@ import PackageDescription
 // Version is technically not required here, SPM doesn't check
 let version = "2.9.1"
 // Tag is required to point towards the right asset. SPM requires the tag to follow semantic versioning to be able to resolve it.
-let tag = "2.9.1"
-let checksum = "9fec2b888e6e2940b1bfbd5d3d010b9f67076b52170923549095cbb74132403b"
-let url = "https://github.com/sparkle-project/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
+let tag = "2.9.1-breakdown.1"
+let checksum = "82a694f9a2d34d19485df4aa8a7890c9725fc95f81900db411346583403c1549"
+let url = "https://github.com/techyjoel/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
 
 let package = Package(
     name: "Sparkle",
